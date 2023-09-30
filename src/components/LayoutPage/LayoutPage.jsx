5ff02ab3134e2ee0cd0f1e5ab28898ed1765c477
Victor Degaspari <victor.degaspari@clinicarx.com.br>
2023-09-30 16:45:31 -0300

    fix css styles

--- src/components/LayoutPage/LayoutPage.jsx
+++ src/components/LayoutPage/LayoutPage.jsx
@@ -5,66 +5,59 @@ function LayoutPage(props) {
   const {
     title,
     subtitle,
-    info,
     bodyStyle,
-    breadcrumbItems,
+    icon,
     background,
     header,
     body,
   } = props;
 
   return (
-    <div
-      className={`w-full container-patients flex flex-col items-center justify-start`}
-    >
+    <div className={`w-full container-patients flex flex-col items-center justify-start`}>
       <div
-        className={`toolbar header-default mobile-flex-column ${
-          breadcrumbItems && breadcrumbItems.length ? "pt-9" : "stick-header"
-        } flex flex-col items-center w-full justify-center`}
-        style={{ background }}
+        className={`toolbar header-default mobile-flex-column !pt-9 stick-header flex flex-col items-center w-full justify-center`}
+        style={{ background: background }}
       >
-        <div
-          className={`flex justify-between items-center w-full max-w-[1320px]`}
-        >
-          <div className="w-full flex items-center justify-start">
-            {/* {breadcrumbItems && breadcrumbItems.length ? <NavigationBread items={breadcrumbItems} /> : null} */}
-          </div>
+        <div className={`flex justify-between items-center w-full max-w-[1320px]`}>
           <div className="toolbar-header flex justify-between items-center w-full">
             <div className="w-full flex items-start">
+              {icon ? (
+                <span
+                  className="mt-1 mr-2 material-icons-outlined cursor-pointer text-white"
+                >
+                  { icon }
+                </span>
+              ) : null}
               <h2 className="text-xl font-bold mb-4 text-white flex-col justify-start">
                 {title}
                 {subtitle ? (
-                  <span className="subtitle text-white">{subtitle}</span>
+                  <div>
+
+                    <span className="subtitle text-white">{subtitle}</span>
+                  </div>
                 ) : null}
               </h2>
-              {info ? (
-                <span
-                  className="mt-1 ml-2 material-icons-outlined cursor-pointer"
-                  data-tippy-content={info}
-                  data-tippy-arrow="true"
-                  data-tippy-placement="bottom"
-                  data-tippy-theme="light bordered"
-                >
-                  info
-                </span>
-              ) : null}
+
             </div>
             <div className="w-full justify-end flex mobile-flex-column" />
           </div>
         </div>
       </div>
-        <div className="w-full pl-5 pr-5 mb-5 flex items-center justify-center">
-          <div className="toolbar-content relative w-full">
-            <div className="card">
-              <div className="card-header mobile-flex-column">{header}</div>
-              <div className="card-body">
-                <div className="w-full overflow-auto" style={{ bodyStyle }}>
-                  {body}
-                </div>
+      
+      <div className="w-full pl-5 pr-5 mb-5 flex items-center justify-center">
+        <div className="toolbar-content relative w-full">
+          <div>
+            <div className="card-header mobile-flex-column">
+              {header}
+            </div>
+            <div className="card-body">
+              <div className="w-full overflow-auto" style={{ bodyStyle }}>
+                {body}
               </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,10 @@
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./index.scss";
-import React, { useState, useEffect } from "react";
 
 import LayoutPage from "../../components/LayoutPage/LayoutPage";
 
 function Home() {
-  const [breadcrumb, setBreadcrumb] = useState([
-    {
-      name: "Home",
-    },
-  ]);
   const [user_type, setUserType] = useState("");
   const [cards, setCards] = useState([]);
   const route = useNavigate();
@@ -19,93 +14,79 @@ function Home() {
     setUserType(user.user_type);
     setCards([
       {
-        name: "Minhas prescrições",
+        name: "Estoque de absorventes",
         icon: "menu_book",
-        description:
-          "Prescrições para seu paciente, você pode gerar receitas ou até anexar vídeos para ele assistir quando quiser!",
+        description: "Absorventes para seu paciente, você pode gerar controlar o estoque de produtos quando quiser!",
         active: true,
         route: "/prescriptions",
       },
       {
-        name: "Agendamentos",
+        name: "Produtos",
         icon: "edit_calendar",
-        description:
-          "Suas reuniões ou anotações ficam aqui, prontas para serem acessadas e podem ser vistas por você ou seu paciente",
+        description: "Seus produtos ficam aqui e podem ser editados, excluídos ou criados por você",
         active: true,
         route: "/appointments",
       },
       {
-        name:
-          user_type === "nutritionist" ? "Meus pacientes" : "Meu nutricionista",
+        name: "Meus pacientes",
         icon: "settings_accessibility",
-        description:
-          "Aqui você controla todos seus pacientes, informações do perfil e muito mais!",
+        description: "Aqui você controla todos seus pacientes, informações do perfil e muito mais!",
         active: true,
         route: "/patient",
       },
       {
-        name: "Atalho do paciente",
+        name: "Parceiros Fluxo Positivo",
         icon: "manage_accounts",
-        description:
-          "Escolha um paciente para deixar como padrão, caso tiver um entre automaticamente nas suas informações quando acessar alguma das opções do Home",
-        active: user_type === "nutritionist",
+        description: "Gerencie os parceiros e marcas para conseguir cadastrar produtos e gerencia-los",
+        active: true,
         route: "",
       },
       {
-        name: "Prescrições padrões",
+        name: "Meu Perfil",
         icon: "settings",
-        description:
-          "Crie prescrições padrões para facilitar a criação de prescrições!",
-        active: user_type === "nutritionist",
+        description: "Edite seu perfil e configurações do mesmo",
+        active: true,
         route: "",
       },
       {
         name: "Em breve...",
         icon: "hourglass_empty",
-        description:
-          "Em breve a FitFlow terá novidades e novas funcionalidades, fique por dentro!",
+        description: "Em breve a FitFlow terá novidades e novas funcionalidades, fique por dentro!",
         active: true,
         route: "",
       },
     ]);
   }, [user_type]);
 
-  // Assuming definePageMeta and useHead are custom hooks or functions
-
   return (
-    
-
-      <LayoutPage
-        title="Dashboard"
-        subtitle="Tudo que você precisa!"
-        info="Aqui você encontra tudo que precisa!"
-        bodyStyle="overflow: initial !important "
-        background= "linear-gradient(180deg, #217AFF 0%, #217AFF 100%)"
-        body={
-          <div className="flex w-full flex-wrap justify-start items-center ">
-            {cards.map(
-              (card, index) =>
-                card.active && (
-                  <div
-                    className="card cursor-pointer mr-3 mt-3 w-full"
-                    key={index}
-                    onClick={() => route.push(card.route)}
-                  >
-                    <div className="mb-3 icon flex items-center justify-center">
-                      <span
-                        className={`material-icons-outlined icon_color_${index}`}
-                      >
-                        {card.icon}
-                      </span>
-                    </div>
-                    <h2>{card.name}</h2>
-                    <p className="description">{card.description}</p>
-                  </div>
-                )
-            )}
-          </div>
-        }
-      ></LayoutPage>
+    <LayoutPage
+      title="Fluxo Positivo"
+      subtitle="Dashboard com tudo que você precisa!"
+      icon="bloodtype"
+      bodyStyle="overflow: initial !important "
+      background="linear-gradient(45deg, #800020, #FF0000)"
+      body={
+        <div className="flex w-full flex-wrap justify-start items-center ">
+          {cards.map(
+            (card, index) => card.active && (
+              <div
+                className="card cursor-pointer mr-3 mt-3"
+                key={ index }
+                onClick={() => route(card.route)}
+              >
+                <div className="mb-3 icon flex items-center justify-center">
+                  <span className={`material-icons-outlined icon_color_${index}`}>
+                    { card.icon }
+                  </span>
+                </div>
+                <h2>{ card.name }</h2>
+                <p className="description">{ card.description }</p>
+              </div>
+            )
+          )}
+        </div>
+      }
+    ></LayoutPage>
   );
 }
 
