43b0cbf8c802d5d0ecbc459092a4d03f1b816582
Victor Degaspari <victor.degaspari@clinicarx.com.br>
2023-10-31 10:32:31 -0300

    fix profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 import { get, update } from '../../api';
 import LayoutPage from "../../components/LayoutPage/LayoutPage";
-import Spinner from "../../components/Spinner/Spinner";
 import "./index.scss";
 
 function Profile() {
-	const [searchQuery, setSearchQuery] = useState('');
+	const [userId, setUserId] = useState(localStorage.getItem('userId'));
 	const [loading, setLoading] = useState(false);
 	const [hasChanges, setHasChanges] = useState(false);
 	const [formData, setFormData] = useState({});
@@ -25,25 +25,46 @@ function Profile() {
 	useEffect(() => {
 		setLoading(true);
 		async function getUser() {
-			const { user } = await get(baseUrl + '/users/find/638fb99cbcef8af3abed238d');
-            delete user.password;
-			setLoading(false);
-			if (!user) return;
-			setFormData(user);
+            try {
+                const { user } = await get(baseUrl + '/users/find/' + userId);
+                if (!user) throw new Error();
+                delete user.password;
+                setLoading(false);
+                if (!user) return;
+                setFormData(user);
+            } catch (error) {
+                setLoading(false);
+                console.error(error);
+                toast.error('Erro ao encontrar o usuário, certifique-se de estar logado!');
+            }
 		}
 		getUser();
-	}, [baseUrl]);
+	}, [baseUrl, userId]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        if (formData?.phone?.length < 3 || formData?.phone?.email < 3 || formData?.name?.length < 3) {
+            toast.error('Preencha os campos com ao menos 3 caracteres!');
+            return;
+        }
         setLoading(true);
-        const { updatedUser } = await update(baseUrl + '/users/update/638fb99cbcef8af3abed238d', formData);
-        setFormData(updatedUser);
-        setLoading(false);
-        setHasChanges(false);
+        try {
+            const { updatedUser } = await update(baseUrl + '/users/update/' + userId, formData);
+            if (!updatedUser) throw new Error();
+            setFormData(updatedUser);
+            setLoading(false);
+            setHasChanges(false);
+            toast.success('Usuário atualizado com sucesso!');
+        } catch (error) {
+            console.error(error);
+            setHasChanges(false);
+            setLoading(false);
+            toast.error('E-mail já cadastrado no sistema!');
+        }
     };
 
   	return (
+    <>
     <LayoutPage
 		title="Meu perfil"
 		subtitle="Gerencie o seu perfil aqui"
@@ -61,15 +82,15 @@ function Profile() {
                 <form onSubmit={(event) => handleSubmit(event)}>
                     <label className="flex flex-col mt-4">
                         Nome:
-                        <input defaultValue={formData.name} required name="name" type="text" placeholder="Seu nome" onChange={handleInputChange}/>
+                        <input defaultValue={formData.name} disabled={loading} required name="name" type="text" placeholder="Seu nome" onChange={handleInputChange}/>
                     </label>
                     <label className="flex flex-col mt-4">
                         E-mail:
-                        <input defaultValue={formData.email} required name="email" type="email" placeholder="email@teste.com" onChange={handleInputChange}/>
+                        <input defaultValue={formData.email} disabled={loading} required name="email" type="email" placeholder="email@teste.com" onChange={handleInputChange}/>
                     </label>
                     <label className="flex flex-col mt-4">
                         Telefone:
-                        <input defaultValue={formData.phone} required name="phone" type="text" placeholder="(XX) XXXXX-XXXX" onChange={handleInputChange}/>
+                        <input defaultValue={formData.phone} disabled={loading} required name="phone" type="text" placeholder="(XX) XXXXX-XXXX" onChange={handleInputChange}/>
                     </label>
                     <div className="flex items-center justify-end mt-4">
                         <button type="button" className="mr-2 btn error" onClick={() => {}}>Cancelar</button>
@@ -82,15 +103,21 @@ function Profile() {
                     </div>
                 </form>
             </div>
-
-            {loading && (
-                <div className="w-full flex items-center justify-center mt-4">
-                <Spinner />
-                </div>
-            )}
-
         </div>
-	}></LayoutPage>);
+	}></LayoutPage>
+    <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="colored"
+    />
+    </>);
 }
 
 export default Profile;
