20bfaada198908791e7a34dcd65f6e93c223a07b
Victor Degaspari <victor.degaspari@clinicarx.com.br>
2023-11-05 15:11:36 -0300

    add stock history

--- src/pages/stock/Stock.jsx
+++ src/pages/stock/Stock.jsx
@@ -9,19 +9,27 @@ import "./index.scss";
 
 function Stock() {
 	const [stockError, setStockError] = useState("");
-	const [searchQuery, setSearchQuery] = useState('');
 	const [data, setData] = useState([]);
+	const [dataHistories, setDataHistories] = useState([]);
 	const [productsData, setProductsData] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [modalOpened, setModalOpened] = useState(false);
+	const [showStock, setShowStock] = useState(true);
+	const [showHistory, setShowHistory] = useState(true);
 	const [modalContent, setModalContent] = useState(null);
 	const baseUrl = process.env.REACT_APP_API_URL;
 
 	useEffect(() => {
 		async function getStocks() {
 			setLoading(true);
-			const { stocks } = await get(baseUrl + '/stocks/get/');
+			const { stocks, stocks_history } = await get(baseUrl + '/stocks/get/');
+
 			setLoading(false);
+
+			if (stocks_history?.length) {
+				setDataHistories(stocks_history);
+			}
+
 			if (!stocks) return;
 			setData(stocks);
 		}
@@ -44,6 +52,7 @@ function Stock() {
 
 	async function deleteStock(stockId) {
 		setData(data.filter(product => product._id !== stockId));
+		setDataHistories(dataHistories.filter(history => history.stock !== stockId));
 		setModalOpened(false);
 		try {
 			await remove(baseUrl + '/stocks/remove/' + encodeURI(stockId));
@@ -64,13 +73,23 @@ function Stock() {
 				jsonData[key] = value;	
 			}
 		});
-
+		// eslint-disable-next-line eqeqeq
+		if (jsonData?.quantity == stock?.quantity) {
+			return toast.error("Quantidade deve ser diferente da atual!");
+		}
+		if (jsonData?.quantity < 0) {
+			return toast.error("Quantidade não pode ser menor que 0");
+		}
         try {
             const response = await update(baseUrl + '/stocks/update/' + encodeURI(stock._id), jsonData);
             if (response.info?.type === 'Error') throw new Error();
 			const index = data.findIndex(item => item._id === response.updatedStock._id);
+
 			const updatedItems = data;
 			updatedItems[index] = response.updatedStock;
+			if (response.stockHistory) {
+				setDataHistories([response.stockHistory, ...dataHistories])
+			}
 			toast.success("Produto atualizado com sucesso!");
 			setData(updatedItems);
             setLoading(false);
@@ -121,18 +140,15 @@ function Stock() {
 		bodyStyle="overflow: initial !important "
 		background="linear-gradient(to right, #a855f7, #a47ffa)"
 		header={
-			<div className="table-header w-full flex items-center justify-between">
-				<div className="relative">
-					<input
-						className="block w-full py-2 px-3 rounded-lg border border-gray-300 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-						type="text"
-						placeholder="Buscar estoque"
-						onChange={() => {}}
-					/>
-					<div className="absolute top-3 right-0 flex items-center pr-3 pointer-events-none">
-						<span className="material-icons-outlined">
-							search
-						</span>
+			<div className="table-header w-full flex items-center justify-between mobile-flex-column">
+				<div className="relative flex items-center mobile-flex-column">
+					<div className="flex items-center mr-4">
+						<input checked={showHistory} onChange={() => setShowHistory(!showHistory)} id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-400 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+						<label htmlFor="default-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-800 select-none">Entradas e saídas</label>
+					</div>
+					<div className="flex items-center">
+						<input checked={showStock} onChange={() => setShowStock(!showStock)} id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-400 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+						<label htmlFor="checked-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-800 select-none">Quantidades atuais</label>
 					</div>
 				</div>
 				<button className="btn mr-2 flex items-center" onClick={() => openModal(
@@ -172,7 +188,7 @@ function Stock() {
       	body={
         <div className="w-full">
             <div>
-            <table className="w-full text-sm text-left text-gray-500">
+			{(showStock || (!showStock && !showHistory)) && <table className="w-full text-sm text-left text-gray-500">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
                         <th className="px-6 py-3" scope="col">
@@ -218,9 +234,9 @@ function Stock() {
                                 <div className="flex items-center justify-end w-full">
                                     <span className="material-icons-outlined mr-2 text-gray-600" onClick={() => openModal(
 										<form onSubmit={(e) => editStock(e, stock)}>
-											<h1>Editar produto no estoque</h1>
+											<h1>Editar quantidade do produto no estoque</h1>
 											<label className="flex flex-col mt-4">
-												Quantidade:
+												<b>Quantidade:</b>
 												<input type="number" name="quantity" defaultValue={stock.quantity} placeholder="Quantidade" />
 											</label>
 											<label className="flex flex-col mt-4">
@@ -249,7 +265,9 @@ function Stock() {
                                     </span>
                                     <span className="material-icons-outlined mr-2 text-red-500" onClick={() => openModal(
 										<div>
-											<span>Deseja realmente excluir esse produto do estoque?</span>
+											<span>Deseja realmente excluir esse estoque?</span>
+											<br />
+											<span>*Atenção! As entradas e saídas desse produto também serão excluídas</span>
 											<div className="flex items-center justify-end mt-4">
 												<button className="mr-2 btn error" onClick={() => setModalOpened(false)}>Cancelar</button>
 												<button className="mr-2 btn" onClick={() => deleteStock(stock._id)}>Confirmar</button>
@@ -263,7 +281,58 @@ function Stock() {
                         </tr>
                     ))}
                     </tbody>
-                </table>
+                </table>}
+				{(showHistory || (!showStock && !showHistory)) && <>
+					<h1 className="text-2xs text-gray-700 uppercase bg-gray-50 mt-4">Entradas e saídas</h1>
+					<table className="w-full text-sm text-left text-gray-500 mt-4">
+						<thead className="text-xs text-gray-700 uppercase bg-gray-50">
+							<tr>
+								<th className="px-6 py-3" scope="col">
+									Quantidade Atual
+								</th>
+								<th className="px-6 py-3" scope="col">
+									Quantia de entrada/saída
+								</th>
+								<th className="px-6 py-3" scope="col">
+									Quantia anterior
+								</th>
+								<th className="px-6 py-3" scope="col">
+									Produto
+								</th>
+								<th className="px-6 py-3" scope="col">
+									Tipo
+								</th>
+								<th className="px-6 py-3" scope="col">
+									Data
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{dataHistories.length > 0 && dataHistories.map((stock, index) => (
+								<tr key={index} className={`border-b-2 border-dashed ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>
+									<td className="px-6 py-4 text-gray-900">
+										<b>{ stock.quantity || 0 }</b> Itens
+									</td>
+									<td className={ `px-6 py-4  ${(stock.quantity > stock.last_quantity ? 'text-green-700' : 'text-red-700')}` }>
+										<b>{ (stock.quantity > stock.last_quantity ? '+' : '') + (stock.quantity - stock.last_quantity)}</b> Itens
+									</td>
+									<td className="px-6 py-4 text-gray-900">
+										<b>{ stock.last_quantity || 0 }</b> Itens
+									</td>
+									<th className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap" scope="row">
+										{ stock.product && <span className="font-normal text-gray-900 whitespace-nowrap text-ellipsis overflow-hidden max-w-md">{ stock.product.name + ' - Tamanho ' + stock.product.size + ' - Tipo ' + (stock.product.type === 'evening' ? 'Noturno' : (stock.product.type === 'internal' ? 'Interno' : 'Diário')) + (stock.product.flap ? ' com aba' : ' sem aba') }</span> }
+									</th>
+									<td className={`px-6 py-4 whitespace-nowrap ${ stock.type === 'outflow' ? 'text-red-700' : 'text-green-700' }`}>
+										<b>{ stock.type === 'outflow' ? 'Saída' : 'Entrada' }</b> 
+									</td>
+									<td className="px-6 py-4 whitespace-nowrap text-gray-900">
+										{  new Intl.DateTimeFormat('pt-BR', {timeStyle: 'medium',dateStyle: 'short',}).format(new Date(stock.created)) }
+									</td>
+								</tr>
+							))}
+                    	</tbody>
+               		</table></>
+				}
             </div>
 
             {loading && (
